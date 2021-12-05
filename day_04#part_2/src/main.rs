8ff987ff3d21c99fb115dd32219554d1a0711896
Luka <git@geluti.org>
2021-12-05 15:22:23 +0100

    Add day 4

---
+++ day_04#part_2/src/main.rs
@@ -0,0 +1,72 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+fn main() {
+  let filename = "../assets/input";
+  let file = File::open(filename).expect("Something went wrong when opening file");
+  let reader = BufReader::new(file);
+
+  let mut drawn: Vec<i32> = Vec::new();
+  let mut boards: Vec<[[i32; 5]; 5]> = Vec::new();
+  let mut board_buffer: [[i32; 5]; 5] = [[0; 5]; 5];
+
+  for (i, line) in reader.lines().enumerate() {
+    let _line = line.expect("Invalid line");
+
+    if i == 0 {
+      drawn = _line.split(',').map(|s| s.parse().expect("Cannot parse")).collect();
+      continue
+    }
+    if i == 1 {
+      continue
+    }
+
+    let board_line = (i as i32 - 2) % 6;
+
+    if board_line == 5 {
+      // Flush the buffer
+      boards.push(board_buffer);
+      continue
+    }
+
+    for (i, n) in _line.split_ascii_whitespace().map(|s| s.parse().expect("Cannot parse")).enumerate() {
+      board_buffer[board_line as usize][i] = n;
+    }
+  }
+  boards.push(board_buffer);
+
+  let ro_boards = boards.clone();
+  let mut boards_success: Vec<bool> = Vec::with_capacity(boards.len());
+
+  for _ in ro_boards.iter() {
+    boards_success.push(false);
+  }
+
+  'find_winner: for num in drawn {
+    for (b, board) in ro_boards.iter().enumerate() {
+      for (i, line) in board.iter().enumerate() {
+        for (j, n) in line.iter().enumerate() {
+          if n == &num {
+            boards[b][i][j] = -n - 1;
+          }
+        }
+      }
+
+      if !boards_success[b]
+        && (boards[b].iter().any(|&line| line.iter().all(|&x| x < 0))
+            || (0..5).any(|i| boards[b].iter().all(|&line| line[i] < 0))) {
+        boards_success[b] = true;
+
+        if boards_success.iter().all(|&b| b) {
+          let score = boards[b].iter().fold(0, |acc, x| acc + x.iter().fold(0, |acc, x| acc + if x > &0 { x } else { &0 }));
+
+          println!("Winner is {}! Score is {} × {} = {}", b, score, num, score*num);
+          println!("Board: {:?}", boards[b]);
+
+          break 'find_winner;
+        }
+      }
+    }
+  }
+
+}
